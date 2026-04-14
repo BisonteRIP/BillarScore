0c1584c371b9bcedd59c968c850e83c04e52c9a5
Vicente Quiroz <99621vicenteaqv@gmail.com>
2026-04-14 19:26:42 -0400

    feat: terminacion de los estilos del main de MainMenu.tsx mas agregado de boton y texto

--- src/pages/MainMenu.tsx
+++ src/pages/MainMenu.tsx
@@ -1,4 +1,5 @@
 import "./MainMenu.css";
+import { Plus } from "lucide-react";
 
 const MainMenu = () => {
   return (
@@ -11,28 +12,32 @@ const MainMenu = () => {
       </div>
 
       {/* CONTENIDO DE LA PÁGINA (Encima de la rejilla y luces) */}
-      <div className="content-wrapper">
-        {/* CABECERA (Header de la imagen) */}
-        <header className="main-header">
-          <div className="brand-section">
-            <div className="brand-icon"></div>
+      <div className="content-wrapper relative z-20">
+        <header className="relative z-10 flex items-center justify-between px-10 py-5 text-xs uppercase tracking-[2px]">
+          <div className="flex items-center gap-2">
+            <div className="w-6 h-6 rounded-full border border-[#14e66c] shadow-[0_0_5px_rgba(20,230,108,0.65)]"></div>
             <span>BILLAR 3 BANDAS</span>
           </div>
-          <div className="version-section">
+          <div className="text-right">
             <span>v1.0 · 2026</span>
           </div>
         </header>
 
         {/* AQUÍ IRÍA EL RESTO DEL MENÚ (TÍTULO Y BOTONES) */}
-        {/* (Eso lo vemos si quieres en la siguiente pregunta) */}
-        <main className="text-white text-center mt-24">
-          {/* Ejemplo temporal para el título central */}
-          <h1 className="uppercase tracking-[2px] font-orbitron font-bold">
+        <main className="flex items-center justify-center flex-col text-center mt-24 text-[#0fa54d] font-orbitron">
+          <p className="uppercase text-xl tracking-[0.4em]">
+            Sistema de gestión
+          </p>
+          <h1 className="uppercase tracking-tighter leading-none mb-2 text-6xl md:text-7xl font-bold bg-linear-to-b from-[#e6e6e6] from-60% to-[#2c2e2f] bg-clip-text text-transparent">
             Torneo
           </h1>
-          <h2 className="text-[#0aff56] uppercase font-orbitron tracking-[4px]">
+          <h2 className="text-[#13ce61] text-4xl uppercase font-semibold tracking-[0.2em] ml-4">
             3 Bandas
           </h2>
+          <button className="flex justify-center items-center gap-4 uppercase rounded-full bg-linear-to-r from-[#19ab4f] to-[#21c15b] text-white py-3 px-10 my-5 font-semibold cursor-pointer hover:brightness-105 transition-all shadow-[0_0_25px_5px_rgba(25,171,79,0.5)]">
+            <Plus className="rounded-full bg-white" color="#19ab4f" size={16} />
+            Nuevo torneo
+          </button>
         </main>
       </div>
     </div>
