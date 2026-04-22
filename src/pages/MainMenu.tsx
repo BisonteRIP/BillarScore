79a99efbd40039f570c02f616707edbf9fde539e
Vicente Quiroz <99621vicenteaqv@gmail.com>
2026-04-22 02:49:44 -0400

    Feat: se termino el apartado de estulo del main menu

--- src/pages/MainMenu.tsx
+++ src/pages/MainMenu.tsx
@@ -1,5 +1,17 @@
 import "./MainMenu.css";
-import { Plus, Trophy} from "lucide-react";
+import { Plus, Trophy } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const MainMenu = () => {
   return (
@@ -34,10 +46,46 @@ const MainMenu = () => {
           <h2 className="text-[#13ce61] text-4xl uppercase font-semibold tracking-[0.2em] ml-4">
             3 Bandas
           </h2>
-          <button className="flex justify-center items-center gap-4 uppercase rounded-full bg-linear-to-r from-[#19ab4f] to-[#21c15b] text-white py-3 px-10 my-5 font-semibold cursor-pointer hover:brightness-105 transition-all shadow-[0_0_25px_5px_rgba(25,171,79,0.5)]">
-            <Plus className="rounded-full bg-white" color="#19ab4f" size={16} />
-            Nuevo torneo
-          </button>
+          <Dialog>
+            <DialogTrigger>
+              <button className="flex justify-center items-center gap-4 uppercase rounded-full bg-linear-to-r from-[#19ab4f] to-[#21c15b] text-white py-3 px-10 my-5 font-semibold cursor-pointer hover:brightness-105 transition-all shadow-[0_0_25px_5px_rgba(25,171,79,0.5)]">
+                <Plus
+                  className="rounded-full bg-white"
+                  color="#19ab4f"
+                  size={16}
+                />
+                Nuevo torneo
+              </button>
+            </DialogTrigger>
+            <DialogContent className="bg-[#0e1c10]" showCloseButton={false}>
+              <DialogHeader>
+                <DialogTitle className="text-xl font-bold font-orbitron text-white">
+                  Nuevo Torneo
+                </DialogTitle>
+                <DialogDescription>
+                  Asigna un nombre para identificar este torneo
+                </DialogDescription>
+              </DialogHeader>
+              <div>
+                <Input
+                  id="input-create-tournament"
+                  placeholder="Ej: Torneo Primavera 2026"
+                  className="border-[#1c4e2d] placeholder:text-[#9ca2ae] bg-[#1c271d] focus-visible:ring-0 focus-visible:ring-offset-0 p-5"></Input>
+              </div>
+              <DialogFooter className="flex items-center justify-center bg-[#0e1c10] border-none">
+                <DialogClose className="flex-1">
+                  <Button
+                    className="cursor-pointer text-[#9ca2ae] bg-[#1c271d] border border-[#1c4e2d] hover:border-[#e7000b] w-full h-10"
+                    variant={"destructive"}>
+                    Cerrar
+                  </Button>
+                </DialogClose>
+                <Button className="flex-1 bg-[#19ab4f5b] text-white cursor-pointer hover:bg-[#19ab4f] p-4 h-10">
+                  Crear & Abrir
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </main>
       </div>
       <div>
